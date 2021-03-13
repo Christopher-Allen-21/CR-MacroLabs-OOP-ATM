f187260ab75765ad3ce80efbe4d2065264c790d4
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-03-12 21:35:19 -0500

    Added withdrawing, opening and closing (Commented out)

--- src/main/java/BankAccounts.java
+++ src/main/java/BankAccounts.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public abstract class BankAccounts {
     double balance = 0.00;
     String status = "Open";
@@ -21,6 +19,46 @@ public abstract class BankAccounts {
 
     public String transferMoney(Account account1, double moneyToTransfer, Account account2) {
         String outputToUser = "";
-    }
 
+    }
+    public void amountWithdrawing(double balance, double amountToWithdraw){
+        if (balance >= amountToWithdraw);
+        balance = balance - amountToWithdraw;
+
+            /* Would like to use this to open new account
+             What languages are being used in Justin's associated accounts
+
+            public int openNewAccount(String customerName, double openingBalance) {
+
+            BankAccount b = new BankAccount(customerName, openingBalance);
+            accounts[numOfAccounts] = b;
+            numOfAccounts++;
+            return b.getAccountNum();
+
+                //Close account
+            public void closeAccount(int accountNo) {
+            accounts[accountNo - 1] = null;
 }
+                //THIS IS CHECKING IF AN ACCOUNT WAS PREVIOUSLY OPENED, (maybe a good feature to add)
+            public int openNewAccount(String customerName, double openingBalance) {
+            BankAccount b = new BankAccount(customerName, openingBalance);
+
+            boolean isAccountFound = false;
+            int i = 0;
+            while(isAccountFound == false && i < accounts.length)
+            {
+                // if() add conditions for this account to replace the old one.
+                // hint: use the isMarkedForDeletion field!
+                // Finally add the new value assignment ( accounts[i] = ... )
+                // return the account number.
+            }
+            // This will happen in the case there were no matches.
+            accounts[numOfAccounts] = b;
+            numOfAccounts++;
+            return b.getAccountNum(); */
+        }
+        }
+
+
+
+
