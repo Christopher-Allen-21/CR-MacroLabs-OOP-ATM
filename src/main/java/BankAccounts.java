873ef1ab4a60103d6c9ba17956ec255ed6db3afb
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-12 21:47:04 -0500

    hey girl" "hey girl" "hey girl" "hey girl" "hey girl" "hey girl" "hey girl" "hey girl" "hey girl"

--- src/main/java/BankAccounts.java
+++ src/main/java/BankAccounts.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public abstract class BankAccounts {
+    User currentUser = new User();
+    //Is that^^^ needed?
+
     double balance = 0.00;
     String status = "Open";
 
@@ -11,16 +14,75 @@ public abstract class BankAccounts {
     public double checkBalance() {
         return balance;
     }
-    public double depositMoney(double deposit) {
-        return balance += deposit;
+    public void depositMoney(double deposit) {
+        balance += deposit;
     }
 
     public void changeStatus(String newStatus) {
         this.status = newStatus;
     }
 
+    //psuedo
     public String transferMoney(Account account1, double moneyToTransfer, Account account2) {
+        /*
+        Needs to take in User account info of account losing money
+        Take in User account that will gain the money
+        Amount of Money to transfer
+         */
+        /*
+            Subtract from first account, add to second account, only if it won't overdraft
+         */
         String outputToUser = "";
+        if (account1 < moneyToTransfer) {
+            outputToUser += "Insufficient funds";
+        } else {
+            account1 -= moneyToTransfer;
+            account2 += moneyToTransfer;
+            outputToUser += "Transfer of $" + moneyToTransfer + " completed";
+        }
+        return outputToUser;
     }
 
+
+    public void amountWithdrawing(double balance, double amountToWithdraw){
+        if (balance >= amountToWithdraw);
+        balance = balance - amountToWithdraw;
+
+            /* Would like to use this to open new account
+             What languages are being used in Justin's associated accounts
+            public int openNewAccount(String customerName, double openingBalance) {
+            BankAccount b = new BankAccount(customerName, openingBalance);
+            accounts[numOfAccounts] = b;
+            numOfAccounts++;
+            return b.getAccountNum();
+                //Close account
+            public void closeAccount(int accountNo) {
+            accounts[accountNo - 1] = null;
+}
+                //THIS IS CHECKING IF AN ACCOUNT WAS PREVIOUSLY OPENED, (maybe a good feature to add)
+            public int openNewAccount(String customerName, double openingBalance) {
+            BankAccount b = new BankAccount(customerName, openingBalance);
+            boolean isAccountFound = false;
+            int i = 0;
+            while(isAccountFound == false && i < accounts.length)
+            {
+                // if() add conditions for this account to replace the old one.
+                // hint: use the isMarkedForDeletion field!
+                // Finally add the new value assignment ( accounts[i] = ... )
+                // return the account number.
+            }
+            // This will happen in the case there were no matches.
+            accounts[numOfAccounts] = b;
+            numOfAccounts++;
+            return b.getAccountNum(); */
+    }
+
+    public void openAccount(String account, User name) {
+        /*
+        Take in string of account type and the User
+        If account already exists, tell user
+        Otherwise, add account type to User map
+         */
+        name.add(account);
+    }
 }
