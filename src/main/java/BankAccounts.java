5df7b1ae6bf7fe94adb052cffd823af271321b99
gregdon13 <71530110+gregdon13@users.noreply.github.com>
2021-03-12 21:52:29 -0500

    Update BankAccounts.java

--- src/main/java/BankAccounts.java
+++ src/main/java/BankAccounts.java
@@ -1,4 +1,7 @@
 public abstract class BankAccounts {
+    User currentUser = new User();
+    //Is that^^^ needed?
+
     double balance = 0.00;
     String status = "Open";
 
@@ -6,35 +9,52 @@ public abstract class BankAccounts {
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
     public double checkBalance() {
         return balance;
     }
-    public double depositMoney(double deposit) {
-        return balance += deposit;
+
+    public void depositMoney(double deposit) {
+        balance += deposit;
     }
 
     public void changeStatus(String newStatus) {
         this.status = newStatus;
     }
 
+    //psuedo
     public String transferMoney(Account account1, double moneyToTransfer, Account account2) {
+        /*
+        Needs to take in User account info of account losing money
+        Take in User account that will gain the money
+        Amount of Money to transfer
+         */
+        /*
+            Subtract from first account, add to second account, only if it won't overdraft
+         */
         String outputToUser = "";
+        if (account1 < moneyToTransfer) {
+            outputToUser += "Insufficient funds";
+        } else {
+            account1 -= moneyToTransfer;
+            account2 += moneyToTransfer;
+            outputToUser += "Transfer of $" + moneyToTransfer + " completed";
+        }
+        return outputToUser;
 
     }
+
     public void amountWithdrawing(double balance, double amountToWithdraw){
         if (balance >= amountToWithdraw);
         balance = balance - amountToWithdraw;
 
             /* Would like to use this to open new account
              What languages are being used in Justin's associated accounts
-
             public int openNewAccount(String customerName, double openingBalance) {
-
             BankAccount b = new BankAccount(customerName, openingBalance);
             accounts[numOfAccounts] = b;
             numOfAccounts++;
             return b.getAccountNum();
-
                 //Close account
             public void closeAccount(int accountNo) {
             accounts[accountNo - 1] = null;
@@ -42,7 +62,6 @@ public abstract class BankAccounts {
                 //THIS IS CHECKING IF AN ACCOUNT WAS PREVIOUSLY OPENED, (maybe a good feature to add)
             public int openNewAccount(String customerName, double openingBalance) {
             BankAccount b = new BankAccount(customerName, openingBalance);
-
             boolean isAccountFound = false;
             int i = 0;
             while(isAccountFound == false && i < accounts.length)
@@ -56,8 +75,8 @@ public abstract class BankAccounts {
             accounts[numOfAccounts] = b;
             numOfAccounts++;
             return b.getAccountNum(); */
-        }
-        }
+    }
+}
 
 
 
